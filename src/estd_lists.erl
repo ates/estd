41cd99958e86eff8a1890f92730f3309d9b15b6f
Artem Teslenko <ates@ipv6.dp.ua>
2012-11-29 13:30:10 +0200

    Add description of estd_lists:merge/2

--- src/estd_lists.erl
+++ src/estd_lists.erl
@@ -2,6 +2,11 @@
 
 -export([merge/2]).
 
+%% @doc Returns the list formed by merging List1 and List2 with replacing
+%% the values of even elements from List2 to List1, also, adds the new
+%% elements from List2 to List1.
+-spec merge([proplists:property()], [proplists:property()]) ->
+    [proplists:property()].
 merge(List1, []) -> List1;
 merge(List1, [{Key, _} = El | Tail]) ->
     NewList = case lists:keymember(Key, 1, List1) of

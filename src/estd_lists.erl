3ecd3afe34e41ce7134d0abb606ced9df329b7ab
Artem Teslenko <ates@ipv6.dp.ua>
2013-09-18 16:17:25 +0300

    Add to_hex/1 and to_hex_string/1 functions

--- src/estd_lists.erl
+++ src/estd_lists.erl
@@ -1,6 +1,8 @@
 -module(estd_lists).
 
 -export([merge/2]).
+-export([to_hex/1]).
+-export([to_hex_string/1]).
 
 %% @doc Returns the list formed by merging List1 and List2 with replacing
 %% the values of even elements from List2 to List1, also, adds the new
@@ -16,3 +18,20 @@ merge(List1, [{Key, _} = El | Tail]) ->
             [El | List1]
     end,
     merge(NewList, Tail).
+
+%% @doc Converts number to hex
+to_hex(N) when N < 256 ->
+    [hex(N div 16), hex(N rem 16)].
+
+%% @doc Converts list or binary to hex string
+-spec to_hex_string(binary()) -> string().
+to_hex_string(Binary) when is_binary(Binary) ->
+    to_hex_string(binary_to_list(Binary));
+
+to_hex_string([]) -> [];
+to_hex_string([H | T]) ->
+    to_hex(H) ++ to_hex_string(T).
+
+%% Internal functions
+hex(N) when N < 10 -> $0 + N;
+hex(N) when N >= 10, N < 16 -> $a + (N - 10).
--- test/estd_lists_tests.erl
+++ test/estd_lists_tests.erl
@@ -6,3 +6,10 @@ merge_test() ->
     List1 = [{a, 10}, {b, 20}, {c, 0}],
     List2 = [{a, 15}, {b, 25}, {d, 30}],
     ?assertEqual(estd_lists:merge(List1, List2), [{d,30},{b,25},{a,15},{c,0}]). 
+
+to_hex_test() ->
+    ?assertEqual(estd_lists:to_hex(111), "6f").
+
+to_hex_string_test() ->
+    ?assertEqual(estd_lists:to_hex_string(<<1,2,15>>), "01020f"),
+    ?assertEqual(estd_lists:to_hex_string([1,2,15]), "01020f").

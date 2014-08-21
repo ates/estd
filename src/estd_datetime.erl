c59d95ace1cdca828c6cc85c456b132b41105ce0
Artem Teslenko <ates@ipv6.dp.ua>
2014-08-21 16:00:40 +0300

    Add datetime_to_binary/{0,1}

--- src/estd_datetime.erl
+++ src/estd_datetime.erl
@@ -1,6 +1,7 @@
 -module(estd_datetime).
 
 -export([midnight/0, uptime/0, timestamp/1, timestamp/2]).
+-export([datetime_to_binary/0, datetime_to_binary/1]).
 
 %% @doc Tell how many seconds left till midnight.
 -spec midnight() -> non_neg_integer().
@@ -26,3 +27,21 @@ timestamp(sec, {MegaSecs, Secs, _MicroSecs}) ->
     MegaSecs * 1000000 + Secs;
 timestamp(msec, {MegaSecs, Secs, MicroSecs}) ->
     (MegaSecs * 1000000 + Secs) * 1000 + (MicroSecs div 1000).
+
+%% @doc Converts the datetime to binary
+-spec datetime_to_binary() -> binary().
+datetime_to_binary() ->
+    datetime_to_binary(calendar:local_time()).
+
+-spec datetime_to_binary(calendar:datetime()) -> binary().
+datetime_to_binary({{YYYY, MM, D}, {H, M, S}}) ->
+    <<(int_fmt(YYYY))/binary, "-", (int_fmt(MM))/binary,
+    "-", (int_fmt(D))/binary, " ", (int_fmt(H))/binary,
+    ":", (int_fmt(M))/binary, ":", (int_fmt(S))/binary>>.
+
+%% Internal functions
+-spec int_fmt(non_neg_integer()) -> binary().
+int_fmt(N) when N < 10 ->
+    <<$0, (integer_to_binary(N))/binary>>;
+int_fmt(N) ->
+    integer_to_binary(N).

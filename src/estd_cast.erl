5b56e6d2af3e719a452ce03162f40802196ffa18
Artem Teslenko <ates@ipv6.dp.ua>
2014-11-29 16:19:22 +0200

    Add more func to estd_cast module

--- src/estd_cast.erl
+++ src/estd_cast.erl
@@ -1,6 +1,10 @@
 -module(estd_cast).
 
 -export([to_ip/1]).
+-export([to_bin/1]).
+-export([to_str/1]).
+-export([to_int/1]).
+-export([to_atom/1]).
 
 -spec to_ip(IP :: tuple() | binary() | string()) -> inet:ip_address().
 to_ip({A, B, C, D} = IP) when
@@ -14,3 +18,36 @@ to_ip(IP) when is_binary(IP) ->
 to_ip(IP) when is_list(IP) ->
     {ok, Address} = inet_parse:address(IP),
     Address.
+
+-spec to_bin(Data :: binary() | list() | atom()) -> binary().
+to_bin(Data) when is_binary(Data) ->
+    Data;
+to_bin(Data) when is_list(Data) ->
+    list_to_binary(Data);
+to_bin(Data) when is_atom(Data) ->
+    atom_to_binary(Data, latin1).
+
+to_str(Data) when is_binary(Data) ->
+    binary_to_list(Data);
+to_str([]) ->
+    "";
+to_str(Data) when is_list(Data) ->
+    Data;
+to_str(Data) when is_integer(Data) ->
+    integer_to_list(Data);
+to_str(Data) when is_atom(Data) ->
+    atom_to_list(Data).
+
+to_int(Data) when is_integer(Data) ->
+    Data;
+to_int(Data) when is_binary(Data) ->
+    binary_to_integer(Data);
+to_int(Data) when is_list(Data) ->
+    list_to_integer(Data).
+
+to_atom(Data) when is_atom(Data) ->
+    Data;
+to_atom(Data) when is_binary(Data) ->
+    binary_to_atom(Data, latin1);
+to_atom(Data) when is_integer(hd(Data)) ->
+    list_to_atom(Data).
--- test/estd_cast_tests.erl
+++ test/estd_cast_tests.erl
@@ -7,3 +7,26 @@ to_ip_test() ->
     ?assertEqual(estd_cast:to_ip("1.1.1.1"), {1, 1, 1, 1}),
     ?assertEqual(estd_cast:to_ip(<<"1.1.1.1">>), {1, 1, 1, 1}),
     ?assertEqual(estd_cast:to_ip("::1"), {0, 0, 0, 0, 0, 0, 0, 1}).
+
+to_bin_test() ->
+    ?assertEqual(estd_cast:to_bin(<<"test">>), <<"test">>),
+    ?assertEqual(estd_cast:to_bin("test"), <<"test">>),
+    ?assertEqual(estd_cast:to_bin(test), <<"test">>),
+    ?assertEqual(estd_cast:to_bin([$t, $e, $s, $t]), <<"test">>).
+
+to_str_test() ->
+    ?assertEqual(estd_cast:to_str("test"), "test"),
+    ?assertEqual(estd_cast:to_str(<<"test">>), "test"),
+    ?assertEqual(estd_cast:to_str(9999), "9999"),
+    ?assertEqual(estd_cast:to_str([]), ""),
+    ?assertEqual(estd_cast:to_str(test), "test").
+
+to_int_test() ->
+    ?assertEqual(estd_cast:to_int("9999"), 9999),
+    ?assertEqual(estd_cast:to_int(<<"9999">>), 9999),
+    ?assertEqual(estd_cast:to_int(9999), 9999).
+
+to_atom_test() ->
+    ?assertEqual(estd_cast:to_atom(<<"test">>), test),
+    ?assertEqual(estd_cast:to_atom("test"), test),
+    ?assertEqual(estd_cast:to_atom(test), test).
